4ac63a6ad12921c719879ba02f134f2b167cad2f
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-10-17 00:29:14 +0200

    Use whatever port is in the environment

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/save/:pagename', authentication, function(req, res){
 });
 
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode",
             app.address().port,
             app.settings.env);
