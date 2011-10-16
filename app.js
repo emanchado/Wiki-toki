7d1f5f0ee8bdc4e4c5324ca8af69f2f13a37f807
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-10-17 00:44:38 +0200

    Rename "conf" to "config"

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var configuration = require('conf');
+var configuration = require('config');
 
 var app = module.exports = express.createServer();
 
