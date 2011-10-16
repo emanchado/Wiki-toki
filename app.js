c64824287bf86e3aaf336dc313dd710bf2aeda69
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-10-16 21:53:39 +0200

    Finish first, simplistic implementation

--- app.js
+++ app.js
@@ -2,11 +2,43 @@ var express = require('express');
 var fs = require('fs');
 
 var configuration = {
-    storeDirectory: 'store'
+  storeDirectory: 'store',
+  secretPassphrase: 'some secret passphrase'
 };
 var wikisyntax = require('./src/wikisyntax.js').wikisyntax;
 
 var app = module.exports = express.createServer();
+
+function pagepath(pagename) {
+  return configuration.storeDirectory + '/' +
+    pagename.replace(/[^a-z0-9]+/gi, '');
+}
+
+function wikiPage(req, res, next) {
+  fs.readFile(pagepath(req.params.pagename), function(err, data) {
+    if (err) {
+      res.render('error', {
+        message: "Couldn't read page " + req.params.pagename
+      });
+    } else {
+      req.pageText = data.toString();
+      next();
+    }
+  });
+}
+
+function authentication(req, res, next) {
+  if (req.session.authenticated === true) {
+    next();
+  } else {
+    if (req.body && req.body.passphrase === configuration.secretPassphrase) {
+      req.session.authenticated = true;
+      next();
+    } else {
+      res.render('login');
+    }
+  }
+}
 
 // Configuration
 app.configure(function(){
@@ -30,7 +62,11 @@ app.configure('production', function(){
 });
 
 // Routes
-app.get('/', function(req, res){
+app.all('/', authentication, function(req, res){
+  res.redirect('/view/WikiIndex');
+});
+
+app.all('/list', authentication, function(req, res){
   fs.readdir(configuration.storeDirectory, function(err, files) {
     if (err) {
       res.render('error', {
@@ -44,32 +80,33 @@ app.get('/', function(req, res){
   });
 });
 
-function wikiPage(req, res, next) {
-  // TODO: Make sure there can't be any '..' or similar in the filename
-  fs.readFile(configuration.storeDirectory + '/' + req.params.pagename, function(err, data) {
-    if (err) {
-      res.render('error', {
-        message: "Couldn't read page " + req.params.pagename
-      });
-    } else {
-      req.pageText = data.toString();
-      next();
-    }
-  });
-}
-
-app.get('/view/:pagename', wikiPage, function(req, res){
+app.all('/view/:pagename', authentication, wikiPage, function(req, res){
   res.render('view', {
     pagename: req.params.pagename,
-    text: wikisyntax(req.pageText)
+    rawText: req.pageText,
+    renderedText: wikisyntax(req.pageText)
   });
 });
 
-app.get('/edit/:pagename', wikiPage, function(req, res){
+app.all('/edit/:pagename', authentication, wikiPage, function(req, res){
   res.render('edit', {
     pagename: req.params.pagename,
-    text: req.pageText
+    rawText: req.pageText
   });
+});
+
+app.post('/save/:pagename', authentication, function(req, res){
+  fs.writeFile(pagepath(req.params.pagename),
+               req.body.rawText,
+               function(err) {
+                 if (err) {
+                   res.render('error', {
+                     message: "Couldn't read page " + req.params.pagename
+                   });
+                 } else {
+                   res.redirect('/view/' + req.params.pagename);
+                 }
+               });
 });
 
 
