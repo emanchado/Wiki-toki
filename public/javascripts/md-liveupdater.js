405c846704c7d13af72ea4db334bd07618dd1e37
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2016-01-24 20:18:06 +0100

    Spinner on image previews
    Add a spinner on image previews for the images that take a long
    time to load.

--- public/javascripts/md-liveupdater.js
+++ public/javascripts/md-liveupdater.js
@@ -1,91 +1,111 @@
 /*global wikisyntax, $ */
 
-function loadImageHandler(imgEl) {
-    return function() {
-        imgEl.data('shouldShow', true);
-        imgEl.data('error', false);
-        imgEl.data('top', $(this).position().top);
-        imgEl.data('left', $(this).position().left);
-        imgEl.attr('src', this.href);
-    };
-}
+var SPINNER_WIDTH = 16,
+    SPINNER_HEIGHT = 16,
+    SPINNER_URL = '/images/ajax-loader.gif';
 
-function hideImageHandler(imgEl) {
-    return function() {
-        imgEl.data('shouldShow', false);
-        imgEl.css('display', 'none');
-    };
-}
+function previewPositionCss(cursorTop, cursorLeft, _imgWidth, imgHeight) {
+    var win = $(window),
+        absoluteCursorTop = win.scrollTop() + cursorTop,
+        distanceTop = cursorTop,
+        distanceBottom = win.height() - cursorTop,
+        positionCss = {left: cursorLeft + 10};
 
-function updateWikiPage(wikiText, targetElement, parserOptions, imgEl) {
-    targetElement.html(wikisyntax(wikiText, parserOptions));
-    targetElement.find('.picture-link').hover(loadImageHandler(imgEl),
-                                              hideImageHandler(imgEl));
-}
-
-function imageError(imgEl) {
-    if ($(imgEl).data('error')) {
-        $(imgEl).data('shouldShow', false);
+    if (distanceBottom > 300 || distanceBottom > distanceTop) {
+        positionCss.top = absoluteCursorTop + 10;
     } else {
-        $(imgEl).data('error', true);
-        $(imgEl).attr('src', '/images/broken-image.png');
+        positionCss.top = absoluteCursorTop - imgHeight - 10;
     }
+    return positionCss;
+}
+
+function resize(imgWidth, imgHeight, maxWidth, maxHeight) {
+    if (imgWidth > maxWidth) {
+        var widthQuo = imgWidth / 300;
+        imgWidth = imgWidth / widthQuo;
+        imgHeight = imgHeight / widthQuo;
+    }
+
+    if (imgHeight > maxHeight) {
+        var heightQuo = imgHeight / 300;
+        imgWidth = imgWidth / heightQuo;
+        imgHeight = imgHeight / heightQuo;
+    }
+
+    return [imgWidth, imgHeight];
 }
 
 function previewImageElement() {
-    return $('<img>').css({
-        position: 'absolute',
-        border: '1px solid black',
-        borderRadius: '3%',
-        backgroundImage: 'url("/images/checkers.jpg")',
+    var previewEl = $('<div>').addClass('image-preview').
+            css('display', 'none').
+            appendTo('body');
+
+    var loadingSpinner = $('<img>').addClass('loading-spinner').
+            attr('src', SPINNER_URL).
+            appendTo(previewEl);
+
+    $('<img>').addClass('image-preview-image').css({
         display: 'none'
     }).load(function() {
-        if (!$(this).data('shouldShow')) {
+        if (! previewEl.data('shouldShow')) {
             return;
         }
 
-        var imgWidth = this.width,
-            imgHeight = this.height;
+        var dim = resize(this.naturalWidth, this.naturalHeight, 300, 300),
+            previewImgWidth = dim[0],
+            previewImgHeight = dim[1];
 
-        if (imgWidth > 300) {
-            var widthQuo = imgWidth / 300;
-            imgWidth = imgWidth / widthQuo;
-            imgHeight = imgHeight / widthQuo;
-        }
-
-        if (imgHeight > 300) {
-            var heightQuo = imgHeight / 300;
-            imgWidth = imgWidth / heightQuo;
-            imgHeight = imgHeight / heightQuo;
-        }
-
-        var imageLinkTop = $(this).data('top'),
-            imageLinkLeft = $(this).data('left'),
-            win = $(window),
-            windowTop = win.scrollTop(),
-            windowBottom = windowTop + win.height(),
-            distanceTop = imageLinkTop - windowTop,
-            distanceBottom = windowBottom - imageLinkTop,
-            positionCss = {left: imageLinkLeft + 20};
-
-        if (distanceBottom > 300 || distanceBottom > distanceTop) {
-            positionCss.top = imageLinkTop + 20;
-        } else {
-            positionCss.top = imageLinkTop - imgHeight - 5;
-        }
-
-        $(this).css(positionCss).css({width: imgWidth,
-                                       height: imgHeight,
-                                       display: ''});
+        loadingSpinner.css('display', 'none');
+        // Need "display: 'block'" explicitly to avoid some annoying
+        // white border below the image
+        $(this).css({width: previewImgWidth,
+                     height: previewImgHeight,
+                     display: 'block'});
+        previewEl.css(previewPositionCss(previewEl.data('cursorTop'),
+                                         previewEl.data('cursorLeft'),
+                                         previewImgWidth,
+                                         previewImgHeight));
     }).error(function() {
-        imageError(this);
-    }).appendTo('body');
+        $(this).attr('src', '/images/broken-image.png');
+    }).appendTo(previewEl);
+
+    return previewEl;
+}
+
+function loadImageHandler(previewEl) {
+    return function(event) {
+        previewEl.data('shouldShow', true);
+        previewEl.data('cursorTop', event.clientY);
+        previewEl.data('cursorLeft', event.clientX);
+
+        previewEl.find('.loading-spinner').css('display', '');
+        previewEl.css('display', '').
+            css(previewPositionCss(previewEl.data('cursorTop'),
+                                   previewEl.data('cursorLeft'),
+                                   SPINNER_WIDTH,
+                                   SPINNER_HEIGHT));
+        previewEl.find('.image-preview-image').css('display', 'none').
+            attr('src', this.href);
+    };
+}
+
+function hideImageHandler(previewEl) {
+    return function() {
+        previewEl.data('shouldShow', false);
+        previewEl.css('display', 'none');
+    };
+}
+
+function updateWikiPage(wikiText, targetElement, parserOptions, previewEl) {
+    targetElement.html(wikisyntax(wikiText, parserOptions));
+    targetElement.find('.picture-link').hover(loadImageHandler(previewEl),
+                                              hideImageHandler(previewEl));
 }
 
 function markdownLiveUpdater(sourceElement, targetElement, parserOptions) {
-    var imgEl = previewImageElement();
+    var previewEl = previewImageElement();
 
-    updateWikiPage(sourceElement.val(), targetElement, parserOptions, imgEl);
+    updateWikiPage(sourceElement.val(), targetElement, parserOptions, previewEl);
 
     sourceElement.bind('input', function() {
         if (targetElement.data('event-state') !== 'buffered') {
@@ -95,7 +115,7 @@ function markdownLiveUpdater(sourceElement, targetElement, parserOptions) {
                 updateWikiPage(sourceElement.val(),
                                targetElement,
                                parserOptions,
-                               imgEl);
+                               previewEl);
             }, 500);
         }
     });
